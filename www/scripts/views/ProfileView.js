61888548112c967d32c8abc5bda4e38ce1725fed
Daniel Costa <costa@vixinet.ch>
2015-10-06 15:27:36 +0300

    CSS first step profile

--- www/scripts/views/ProfileView.js
+++ www/scripts/views/ProfileView.js
@@ -17,8 +17,8 @@ define([
 		events: {
 			'click .report': 'report',
 			'click .certifications': 'certifications',
-			'click .profile-picture': 'profile',
-			'click .my-boat-details': 'boat'
+			'click .open-profile': 'profile',
+			'click .boat': 'boat'
 		},
 		
 		profiles: {},
@@ -29,12 +29,7 @@ define([
 
 			Parse.Analytics.track('profile-click-report');
 
-			var m = new ReportModel({
-				action: 'profile',
-				profile: this.model
-			});
-
-			this.modal(new ReportView({ model : m }));
+			this.modal(new ReportView({ model : new ReportModel({ action: 'profile', profile: this.model }) }));
 		},
 
 		certifications: function(event) {
@@ -55,9 +50,10 @@ define([
 
 		boat: function(event) {
 			event.preventDefault();
-			this.modal(new BoatView({ model: this.boats[$(event.currentTarget).attr('data-id')] }));
-			//alert($(event.currentTarget).attr('data-id'));
 
+			Parse.Analytics.track('profile-click-boat');
+
+			this.modal(new BoatView({ model: this.boats[$(event.currentTarget).attr('data-id')] }));
 		},
 
 		render: function() {
@@ -85,7 +81,6 @@ define([
 					var displayed = 0;
 
 					_.each(requests, function(request) {
-						
 						if( request.get('reviewGuest') != "" ) {
 							displayed++;
 							self.profiles[request.get('profile').id] = request.get('profile');
@@ -94,12 +89,10 @@ define([
 					});
 
 					if( displayed == 0 ) {
-						self.$el.find('.reviews').html('<p class="text-center">No reviews for this host</p>');
+						self.$el.find('.reviews').html('<p class="text-center no-data">No current ratings</p>');
 						return;
 					} 
-					// else {
-					// 	self.$el.find('.reviews').prepend('<h5>Reviews</h5>');
-					// }
+
 				});
 
 				var boatquery = new Parse.Query(Parse.Object.extend('Boat'));
@@ -108,14 +101,13 @@ define([
 				boatquery.find().then(function(boats) {
 					_.each(boats, function(boat) {
 						self.boats[boat.id] = boat;
-						self.$el.find('.my-boats .boat-details').append(_.template(ProfileBoatTemplate)({ model:boat }));
+						self.$el.find('.boats .list').append(_.template(ProfileBoatTemplate)({ model: boat }));
 
 						var boatPictures = boat.relation('boatPictures').query();
 						boatPictures.ascending('order');
 						boatPictures.first().then(function(fileholder) {
 							if( fileholder ) {
-								var target =  self.$el.find('.boat-details .boat-image');
-								target.html(fileholder.get("file").url());
+								self.$el.find('.boats .list .boat[data-id="'+boat.id+'"] .boat-image').html(fileholder.get("file").url());
 							}
 						});
 					});
