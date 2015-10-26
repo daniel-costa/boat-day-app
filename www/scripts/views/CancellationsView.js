37a878f92b2eca771226e821607ac9fb01d60b46
Bibash Shah <shahbibash@gmail.com>
2015-10-26 14:24:07 +0200

    cancellation policy changes and icon changes in boat days

--- www/scripts/views/CancellationsView.js
+++ www/scripts/views/CancellationsView.js
@@ -14,6 +14,24 @@ define([
 
 			this.$el.find('.' + this.model.get('cancellationPolicy')).addClass('active');
 
+			if(this.model.get('cancellationPolicy') == 'flexible') {
+				this.$el.find('.flexiable').show();
+				this.$el.find('.moderate').hide();
+				this.$el.find('.strict').hide();
+			}
+
+			if(this.model.get('cancellationPolicy') == 'moderate') {
+				this.$el.find('.moderate').show();
+				this.$el.find('.flexiable').hide();
+				this.$el.find('.strict').hide();
+			}
+
+			if(this.model.get('cancellationPolicy') == 'strict') {
+				this.$el.find('.strict').show();
+				this.$el.find('.flexiable').hide();
+				this.$el.find('.moderate').hide();
+			}
+
 			return this;
 
 		}
