ad003b1b5684dde7a8547a6bb1c225f813c11483
Daniel Costa <costa@vixinet.ch>
2015-11-27 15:37:16 +0200

    Android Changes

--- pluginsBase/com.boat.day.BDHelper/src/ios/AppDelegate+notification.h
+++ pluginsBase/com.boat.day.BDHelper/src/ios/AppDelegate+notification.h
@@ -2,7 +2,6 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate (notification)
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions;
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken;
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error;
 - (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings;
