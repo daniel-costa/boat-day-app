6a3267c73e3bccfef86cb72d4405068de204e091
Daniel Costa <costa@vixinet.ch>
2015-11-27 19:54:06 +0200

    BDHelper for android

--- pluginsBase/com.boat.day.BDHelper/src/android/BDHelper.java
+++ pluginsBase/com.boat.day.BDHelper/src/android/BDHelper.java
@@ -3,12 +3,12 @@ package com.boat.day;
 import android.app.Application;
 import android.util.Log;
 
-import java.util.Set;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.parse.Parse;
 import com.parse.ParseInstallation;
@@ -16,15 +16,16 @@ import com.parse.PushService;
 
 public class BDHelper extends CordovaPlugin {
 
-    public static final String TAG = "BDHelper";
+    public static final String TAG = "ParsePlugin";
     public static final String ACTION_INITIALIZE = "initialize";
     public static final String ACTION_GET_INSTALLATION_ID = "getInstallationId";
     public static final String ACTION_GET_INSTALLATION_OBJECT_ID = "getInstallationObjectId";
+
     
     public static void initializeParseWithApplication(Application app) {
         String appId = getStringByKey(app, "parse_app_id");
         String clientKey = getStringByKey(app, "parse_client_key");
-
+        
         Parse.enableLocalDatastore(app);
         Parse.initialize(app, appId, clientKey);
 
@@ -57,17 +58,25 @@ public class BDHelper extends CordovaPlugin {
     private void initialize(final CallbackContext callbackContext) {
         cordova.getThreadPool().execute(new Runnable() {
             public void run() {
-                PushService.setDefaultPushCallback(cordova.getActivity(), cordova.getActivity().getClass());
+
                 ParseInstallation.getCurrentInstallation().saveInBackground();
-                // ToDo send JSON
-                /*
-                    {
-                        parseAppId: getStringByKey(app, "parse_app_id")
-                        parseJavaScriptKey: getStringByKey(app, "parse_javascript_key")
-                    }
-                */
-                    
-                callbackContext.success();
+
+                JSONObject jo = new JSONObject();
+                
+                try {
+                    Application gApp  = cordova.getActivity().getApplication();
+
+                    jo.put("parseAppId", getStringByKey(gApp, "parse_app_id"));
+                    jo.put("parseJavaScriptKey", getStringByKey(gApp, "parse_javascript_key"));
+
+                    JSONArray ja = new JSONArray();
+                    ja.put(jo);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                callbackContext.success(jo);
             }
         });
     }
